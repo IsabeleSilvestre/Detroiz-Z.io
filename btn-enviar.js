47d10523960af7a5ab93dbfb99079c323d8b7700
IsabeleSilvestre <isabele.silvestre.27@gmail.com>
2025-04-24 15:28:57 -0300

    Salvando alterações locais antes do pull

--- btn-enviar.js
+++ btn-enviar.js
@@ -1,12 +1,19 @@
 link.rel = 'stylesheet';
 link.href = 'style.css';
+ 
+document.addEventListener("DOMContentLoaded", function () {
+    const botao = document.querySelector(".botao");
 
-const botao = document.querySelector(".botao");
-
-botao.onclick = function(){
-    this.innerHTML = "<div class='bolinha'></div>";
-    setTimeout(() => {
-        this.innerHTML = "Enviado!";
-        this.style = "background:rgb(27, 165, 87); color: #ffffff; pointer-events: none;";
-    }, 3000);
-}   
+    if (botao) {
+        botao.onclick = function (event) {
+            event.preventDefault(); // Impede o envio do formulário e recarregamento da página
+            this.innerHTML = "<div class='bolinha'></div>";
+            setTimeout(() => {
+                this.innerHTML = "Enviado!";
+                this.style = "background:rgb(27, 165, 87); color: #ffffff; pointer-events: none;";
+            }, 3000);
+        };
+    } else {
+        console.error("Botão não encontrado!");
+    }
+});
