331b4df9f745d849ec2837b5a4ec31662d24d957
Alice <alicerodrigueslobo01@gmail.com>
2025-04-25 02:31:57 -0300

    adicionando o php

---
+++ detroit.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Tempo de geração: 25/04/2025 às 04:42
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `detroit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `codigo` int(11) NOT NULL,
+  `nome` varchar(40) NOT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `produto`
+--
+
+INSERT INTO `produto` (`codigo`, `nome`, `quantidade`) VALUES
+(1, 'Z-Bot Assistente Pessoal', 200),
+(2, 'RoboLine Industrial', 200),
+(3, 'Z-Sentinel', 200),
+(4, 'EduBot', 200),
+(5, 'Z-Car Companion', 200);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ produtos.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BANCO</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+</head>
+<body class="corpo">
+
+    <h1 class="edp">ESTOQUE DE PRODUTOS</h1>
+<table border="1" class="tabela">
+        <thead>
+            <tr class="azul">
+                <td><strong>Código</strong></td>
+                <td><strong>Nome</strong></td>
+                <td><strong>Quantidade</strong></td>
+            </tr>
+        </thead>
+        <tbody class="corpo">
+            <?php
+            $conexao = new mysqli("localhost", "root", "", "detroit", 3307); /* conexão */
+            $consulta = $conexao->query("select * from produto");
+            $resultado = $consulta->fetch_all(MYSQLI_ASSOC);
+            foreach ($resultado as $linha)
+                echo ("<tr><td>" . $linha['codigo'] . "</td><td>" . $linha['nome']
+                    . "</td><td>" . $linha['quantidade'] . "</td></tr>");
+            $conexao->close();
+            ?>
+        </tbody>
+</table>
+</body>
+</html>
